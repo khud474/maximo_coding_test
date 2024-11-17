a1338dc7d026e1966c90e79826fe85169b38d276
Kevin Hudson <k.hudson474@gmail.com>
2024-11-17 14:37:37 -0700

    working poc

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cncdev/maximo_coding_test/src
+# Build directory: /home/cncdev/maximo_coding_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/cncdev/maximo_coding_test/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/cncdev/maximo_coding_test/devel;/opt/ros/noetic'.split(';') if '/home/cncdev/maximo_coding_test/devel;/opt/ros/noetic' != '' else []
